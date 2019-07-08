b368cf6e11355a98283771dd1540bc1f6d4a0f95
Jonny Haldas <jonathan.haldas@gmail.com>
2019-07-08 15:22:50 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+FILE_PATH = "/Users/######/Documents/Python/Logs/" # Set this to the path that you want your XML files to be saved in
+
+MAX_SITEMAPS = 5 # Set this to the maximum number of sitemaps you want saved at once
+
+URL = "https://www.Website.com/sitemap.xml" # Set this to the URL of the sitemap
+
+SEND_EMAIL = True # Set True if you want to be emailed, False if not.
